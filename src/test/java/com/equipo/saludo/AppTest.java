8c9bbf8f6d4940e3d0608d455f5f8dff7bddd4e8
jprivas30 <juanpr2512@gmail.com>
2025-06-12 21:46:41 -0400

    Proyecto Java Maven SaludoApp

---
+++ src/test/java/com/equipo/saludo/AppTest.java
@@ -0,0 +1,11 @@
+package com.equipo.saludo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+    @Test
+    public void testSaludo() {
+        assertEquals("¡Hola, DevOps!", App.saludar("DevOps"));
+    }
+}
